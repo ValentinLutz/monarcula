1acdb3cd30f11f92e317bb0e2d766ab0ebf8cbfc
Valentin Lutz <allround.email@gmail.com>
2020-04-02 11:57:50 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.18"
+    java
+}
+
+group = "net.bounceme.monkee"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.3.4"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+
+    """)
+}
